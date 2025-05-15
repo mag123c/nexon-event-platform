a971ba5bc1d876d60a13e6b5e3a8d6fc2d1e9ba9
mag123c <diehreo@gmail.com>
2025-05-15 21:25:07 +0900

    feat: (common) added moongose, config

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -7,9 +7,16 @@ import { UserMongoRepository } from '@app/auth/infrastructure/persistence/user-m
 import { HASHING_PORT } from '@app/auth/domain/ports/hasing.port';
 import { Argon2Service } from '@app/auth/infrastructure/hasing/argon2.service';
 import { User, UserSchema } from '@app/auth/domain/entities/user.entity';
+import { CommonMongooseModule } from '@app/common/database/moongoose/common-mongoose.module';
+import { CommonConfigModule } from '@app/common/config/common-config.module';
 
 @Module({
   imports: [
+    CommonConfigModule,
+    CommonMongooseModule.forRootAsync({
+      configKey: 'AUTH_MONGODB_URI',
+      connectionName: 'auth',
+    }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AuthController],
---
+++ libs/common/src/database/moongoose/common-mongoose.module.ts
@@ -0,0 +1,37 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+export interface CommonMongooseModuleOptions {
+  connectionName?: string;
+  configKey: string;
+}
+
+@Module({})
+export class CommonMongooseModule {
+  static forRootAsync(options: CommonMongooseModuleOptions): DynamicModule {
+    return {
+      module: CommonMongooseModule,
+      imports: [
+        ConfigModule,
+        MongooseModule.forRootAsync({
+          connectionName: options.connectionName,
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => {
+            const uri = configService.get<string>(options.configKey);
+            if (!uri) {
+              throw new Error(
+                `URI가 정의되지 않았습니다. configKey: ${options.configKey}`,
+              );
+            }
+            return {
+              uri,
+            };
+          },
+          inject: [ConfigService],
+        }),
+      ],
+      exports: [MongooseModule],
+    };
+  }
+}
